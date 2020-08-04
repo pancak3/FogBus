7a5611af5364fc1f51b7934ff873372aba8ce702
Qifan Deng <plocircle@live.com>
2020-08-04 15:58:34 +0800

    Add Alert Dialog

--- iOS-app/fog_app/lib/main.dart
+++ iOS-app/fog_app/lib/main.dart
@@ -3,8 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,161 +10,114 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'HealthKeeper',
-        theme: new ThemeData(
-          primaryColor: Colors.white,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("HealthKeeper"),
-          ),
-          body: Center(
-              child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: Text(
-                  "Welcome to",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Text(
-                  "HealthKeeper",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Text(
-                  "Your health manager using Fog network",
-                ),
-              ),
-              const SizedBox(height: 70),
-              RaisedButton(
-                onPressed: () {},
-                child:
-                    const Text('Pair Sensor', style: TextStyle(fontSize: 20)),
-              ),
-              const SizedBox(height: 70),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Text(
-                  "Enter address of Master: ",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-              Container(
-                width: 200,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(hintText: 'Master Address'),
-                ),
-              ),
-              // const SizedBox(height: 30),
-              RaisedButton(
-                onPressed: () {},
-                child: const Text('Connect', style: TextStyle(fontSize: 20)),
-              ),
-              const SizedBox(height: 80),
-              FlatButton(
-                onPressed: () {
-                  /*...*/
-                },
-                child: Text(
-                  "Remove all saved devices",
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 30),
-                child: Text(
-                  "Developed by CLOUDS Lab",
-                ),
-              ),
-            ],
-          )),
-        ));
+      title: 'HealthKeeper',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: MyHome(),
+    );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Name Generator'),
-          actions: <Widget>[
-            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
-          ],
-        ),
-        body: _buildSuggestions());
-  }
-
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  final Set<WordPair> _saved = new Set<WordPair>();
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
+      appBar: AppBar(
+        title: Text("HealthKeeper"),
       ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Text(
+              "Welcome to",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "HealthKeeper",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "Your health manager using Fog network",
+            ),
+          ),
+          const SizedBox(height: 70),
+          RaisedButton(
+            onPressed: () {
+              showAlertDialog(context);
+            },
+            child: const Text('Pair Sensor', style: TextStyle(fontSize: 20)),
+          ),
+          const SizedBox(height: 70),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "Enter address of Master: ",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Container(
+            width: 200,
+            child: TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(hintText: 'Master Address'),
+            ),
+          ),
+          // const SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {},
+            child: const Text('Connect', style: TextStyle(fontSize: 20)),
+          ),
+          const SizedBox(height: 80),
+          FlatButton(
+            onPressed: () {
+              /*...*/
+            },
+            child: Text(
+              "Remove all saved devices",
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: Text(
+              "Developed by CLOUDS Lab",
+            ),
+          ),
+        ],
+      )),
     );
   }
+}
 
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(new MaterialPageRoute<void>(builder: (BuildContext context) {
-      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
-        return new ListTile(
-          title: new Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      });
-      final List<Widget> divided = ListTile.divideTiles(
-        tiles: tiles,
-        context: context,
-      ).toList();
+showAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () => Navigator.pop(context, true),
+  );
 
-      return new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Saved Suggestions'),
-        ),
-        body: new ListView(children: divided),
-      );
-    }));
-  }
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Success!"),
+    content: Text("Connected to Virtual Device."),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
